9819f53cff0812b89f25ade80445f352e1169b04
Brian Wu <brianwu02@gmail.com>
2014-03-27 15:47:25 -0400

    add client

---
+++ harmony/networking/net-watcher-json-client.js
@@ -0,0 +1,14 @@
+var net = require('net');
+var client = net.connect({port:5432});
+
+client.on('data', function(data) {
+  var message = JSON.parse(data);
+  if (message.type === 'watching') {
+    console.log('Now Watching: ' + message.file);
+  } else if (message.type === 'changed') {
+    var date = new Date(message.timestamp);
+    console.log("file " + message.file + " changed at " + date);
+  } else {
+    throw Error("uncrecognized message type: " + message.type);
+  }
+});
