2febe5d53a2414cf19a8bb5f4a002e6561ead76e
Brian Wu <brianwu02@gmail.com>
2014-04-01 13:50:59 -0400

    created series version of job execution

--- harmony/messaging/jobCreater.js
+++ harmony/messaging/jobCreater.js
@@ -4,10 +4,24 @@ var async = require('async');
 var jobs = [];
 var numbers = _.range(1000);
 
+var seriesJobs = [];
+var seriesNumbers = _.range(30);
+
 /*  builds function from numbers range which when executed returns the square root.
  *  pushes each function on to jobs queue
  *  execute all functions in parallel
  */
+
+
+seriesNumbers.forEach(function(data) {
+  seriesJobs.push(function() {
+    return Math.pow(data, 2);
+  });
+});
+
+seriesJobs.forEach(function(fn) {
+  console.log(fn());
+});
 
 
 numbers.forEach(function(data) {
@@ -23,3 +37,4 @@ async.parallel(jobs, function(err, results) {
   }
   console.log(results);
 });
+
